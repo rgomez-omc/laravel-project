daa5fe6174a9bfa38433538bfe0644ab39d77047
Rudy Gomez <rudy@omcnet.net>
2020-06-26 15:39:56 -0400

    Home Fix 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,7 @@
         <div id="banner" class="container">
             <h2>
 
-                Maecenas
-                luctus
-                lectus
+                New Home Header on Dev
 
             </h2>
             <p>This is <strong>SimpleWork</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
